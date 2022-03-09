3d643df4503040d21fba0861e0919a946bb92007
root <root@soumns.localdomain>
2022-03-09 22:39:19 +0800

    :wrench: perf:change-config(修改vite.config.ts配置文件)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
+  <div class="container">999</div>
 </template>
 
 <style lang="scss" scoped>
-
+/* 引入sass变量 */
+.container {
+  background-color: $background-color;
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import { resolve } from 'path';
+
+function pathResolve(dir: any) {
+  return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      // @/xx => src/xx
+      {
+        find: /@\//,
+        replacement: pathResolve('src') + '/',
+      },
+
+      // /#/xx => types/xx
+      {
+        find: /#\//,
+        replacement: pathResolve('types') + '/',
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/sass/mixin.scss";`, // 这里结尾需要添加一个 ;
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    open: true, // 是否自动在浏览器打开
+    // 反向代理解决跨域
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:3001',
+        target: 'http://v.juhe.cn',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
