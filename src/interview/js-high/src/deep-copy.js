c5d83a8a9556f59920fe5d811c82d4bcc24c9069
soumns <2794122209@qq.com>
2022-05-19 15:11:53 +0800

    ✨ feat:add-shallow-deep-clone

--- src/interview/js-high/src/deep-copy.js
+++ src/interview/js-high/src/deep-copy.js
@@ -45,7 +45,31 @@ let arr = {
 let arr1 = JSON.parse(JSON.stringify(arr));
 
 arr['age'] = 99;
-console.log(arr, arr1, '💙💛 深拷贝 JSON.parse(JSON.stringify())');
+console.log(arr1, '💙💛 深拷贝 JSON.parse(JSON.stringify())');
 
 // ②、cloneDeep  lodash
-console.log(arr === cloneDeep(arr), '💙💛 cloneDeep'); // false
+let arr2 = cloneDeep(arr);
+
+arr['name'] = 'iu';
+console.log(arr2, '💙💛 cloneDeep'); // {change:()={},gender:'男' }
+
+// ③、手动递归循环 深拷贝
+function deepClone(obj, hash = new WeakMap()) {
+  if (obj === null) return obj; // 如果是null或者undefined我就不进行拷贝操作
+  if (obj instanceof Date) return new Date(obj);
+  if (obj instanceof RegExp) return new RegExp(obj);
+  // 可能是对象或者普通的值  如果是函数的话是不需要深拷贝
+  if (typeof obj !== 'object') return obj;
+  // 是对象的话就要进行深拷贝
+  if (hash.get(obj)) return hash.get(obj);
+  let cloneObj = new obj.constructor();
+  // 找到的是所属类原型上的constructor,而原型上的 constructor指向的是当前类本身
+  hash.set(obj, cloneObj);
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      // 实现一个递归拷贝
+      cloneObj[key] = deepClone(obj[key], hash);
+    }
+  }
+  return cloneObj;
+}
